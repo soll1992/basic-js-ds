c10cf4c03b81668b57f9ea0b472890a97634df76
Anton <dr.dud34@gmail.com>
2021-10-18 23:11:14 +0300

    feat: queue complite

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-module.exports = class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ module.exports = class Queue {
+  constructor() {
+    this.list = null
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  enqueue(element) {
+    let node = new ListNode(element) 
+      if (this.list === null) {
+        this.list = node
+      } else {
+        let end = this.list
+        while(end.next !== null) {
+          end = end.next
+        }
+        end.next = new ListNode(element)
+      }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let value = this.list.value;
+    this.list = this.list.next;
+    return value;
   }
 
+  getUnderlyingList() {
+    return this.list
+  }
 }
+
+
