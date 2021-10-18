a7efa629342b3de017d1491ad04a844b6cd1d1b3
Anton <dr.dud34@gmail.com>
2021-10-18 22:30:49 +0300

    feat: stack complete

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,18 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.newArr = []
+  }
+  push(element) {
+    this.newArr.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.newArr.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.newArr[this.newArr.length - 1]
   }
 }
